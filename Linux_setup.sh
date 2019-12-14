9a2c8b86505162f2c1f171c58f655a172a9d1087
jdhao <jdhao@hotmail.com>
2019-12-14 13:20:57 +0800

    Fix minor issues

--- Linux_setup.sh
+++ Linux_setup.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 set -eux
 set -o pipefail
 
@@ -14,7 +14,7 @@ LIBEVENT_PACK_DIR=$HOME/packages/libevent
 LIBEVENT_LINK="https://github.com/libevent/libevent/releases/download/release-2.1.10-stable/libevent-2.1.10-stable.tar.gz"
 
 if [[ ! -f $LIBEVENT_SRC_NAME ]]; then
-    wget $LIBEVENT_LINK -O $LIBEVENT_SRC_NAME
+    wget "$LIBEVENT_LINK" -O "$LIBEVENT_SRC_NAME"
 fi
 
 echo "Building libevent"
@@ -24,12 +24,12 @@ if [[ ! -d "$LIBEVENT_PACK_DIR" ]]; then
 fi
 
 echo "Extracting to $HOME/packages/libevent directory"
-tar zxvf $LIBEVENT_SRC_NAME -C $LIBEVENT_PACK_DIR --strip-components 1
-cd $LIBEVENT_PACK_DIR
+tar zxvf "$LIBEVENT_SRC_NAME" -C "$LIBEVENT_PACK_DIR" --strip-components 1
+cd "$LIBEVENT_PACK_DIR"
 
 ./configure --prefix="$HOME/local"
-make -j$(nproc) && make install
-cd $HOME
+make -j && make install
+cd "$HOME"
 
 # Building ncurses
 NCURSES_SRC_NAME=$HOME/packages/ncurses.tar.gz
@@ -37,7 +37,7 @@ NCURSE_PACK_DIR=$HOME/packages/ncurses
 NCURSES_LINK="ftp://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz"
 
 if [[ ! -f $NCURSES_SRC_NAME ]]; then
-    wget $NCURSES_LINK -O $NCURSES_SRC_NAME
+    wget "$NCURSES_LINK" -O "$NCURSES_SRC_NAME"
 fi
 
 echo "Building ncurses"
@@ -47,12 +47,12 @@ if [[ ! -d "$NCURSE_PACK_DIR" ]]; then
 fi
 
 echo "Extracting to $HOME/packages/ncurses directory"
-tar zxvf $NCURSES_SRC_NAME -C $NCURSE_PACK_DIR --strip-components 1
-cd $NCURSE_PACK_DIR
+tar zxvf "$NCURSES_SRC_NAME" -C "$NCURSE_PACK_DIR" --strip-components 1
+cd "$NCURSE_PACK_DIR"
 
 ./configure --prefix="$HOME/local" CXXFLAGS="-fPIC" CFLAGS="-fPIC"
-make -j$(nproc) && make install
-cd $HOME
+make -j && make install
+cd "$HOME"
 
 # Building tmux
 TMUX_SRC_NAME=$HOME/packages/tmux.tar.gz
@@ -60,7 +60,7 @@ TMUX_PACK_DIR=$HOME/packages/tmux
 TMUX_LINK="https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz"
 
 if [[ ! -f $TMUX_SRC_NAME ]]; then
-    wget $TMUX_LINK -O $TMUX_SRC_NAME
+    wget "$TMUX_LINK" -O "$TMUX_SRC_NAME"
 fi
 
 echo "Building Tmux"
@@ -70,15 +70,15 @@ if [[ ! -d "$TMUX_PACK_DIR" ]]; then
 fi
 
 echo "Extracting to $HOME/packages/tmux directory"
-tar zxvf $TMUX_SRC_NAME -C $TMUX_PACK_DIR --strip-components 1
-cd $TMUX_PACK_DIR
+tar zxvf "$TMUX_SRC_NAME" -C "$TMUX_PACK_DIR" --strip-components 1
+cd "$TMUX_PACK_DIR"
 
 ./configure --prefix="$HOME/local" \
     CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" \
     LDFLAGS="-L$HOME/local/lib"
-    make -j$(nproc) && make install
+    make -j && make install
 
-cd $HOME
+cd "$HOME"
 
 #######################################################################
 #                            Building zsh                             #
@@ -90,7 +90,7 @@ ZSH_PACK_DIR=$HOME/packages/zsh
 ZSH_LINK="https://sourceforge.net/projects/zsh/files/latest/download"
 
 if [[ ! -f $ZSH_SRC_NAME ]]; then
-    wget $ZSH_LINK -O $ZSH_SRC_NAME
+    wget "$ZSH_LINK" -O "$ZSH_SRC_NAME"
 fi
 
 echo "Creating zsh directory under packages directory"
@@ -98,15 +98,15 @@ if [[ ! -d "$ZSH_PACK_DIR" ]]; then
     mkdir -p "$ZSH_PACK_DIR"
 fi
 
-tar xJvf $ZSH_SRC_NAME -C $ZSH_PACK_DIR --strip-components 1
-cd $ZSH_PACK_DIR
+tar xJvf "$ZSH_SRC_NAME" -C "$ZSH_PACK_DIR" --strip-components 1
+cd "$ZSH_PACK_DIR"
 
 ./configure --prefix="$HOME/local" \
     CPPFLAGS="-I$HOME/local/include" \
     LDFLAGS="-L$HOME/local/lib"
-make -j$(nproc) && make install
+make -j && make install
 
-cd $HOME
+cd "$HOME"
 
 #######################################################################
 #                         Install zsh plugins                         #
@@ -116,9 +116,10 @@ cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
 # Download rc files
-curl -Lo $HOME/.bash_profile https://raw.githubusercontent.com/jdhao/dotfiles/master/shell/.bash_profile \
-    && curl -Lo $HOME/.zshrc https://raw.githubusercontent.com/jdhao/dotfiles/master/shell/.zshrc \
-    && curl -Lo $HOME/.pylintrc https://raw.githubusercontent.com/jdhao/dotfiles/master/pylint/.pylintrc
+curl -Lo "$HOME/.bash_profile" https://raw.githubusercontent.com/jdhao/dotfiles/master/shell/.bash_profile \
+    && curl -Lo "$HOME/.zshrc" https://raw.githubusercontent.com/jdhao/dotfiles/master/shell/.zshrc \
+    && curl -Lo "$HOME/.pylintrc" https://raw.githubusercontent.com/jdhao/dotfiles/master/pylint/.pylintrc
 
 # Initialize zsh and install plugins
-source $HOME/.bash_profile
+# shellcheck source=/dev/null
+source "$HOME/.bash_profile"
