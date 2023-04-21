25fb06be6e1dca6c4df09cbb8f5b79fec58fd345
jdhao <jdhao@hotmail.com>
2023-04-21 22:24:03 +0200

    add macos setup script

---
+++ mac_setup.sh
@@ -0,0 +1,25 @@
+# install homebrew first
+
+# command lines tools
+brew install \
+  neovim \
+  node \
+  ctags \
+  lua-language-server \
+  stylua \
+  ripgrep \
+  pandoc \
+  fzf \
+  ffmpeg \
+  fontconfig \
+  hugo \
+  miniconda \
+  tmux
+
+# HomeBrew casks
+brew install --cask \
+  skim \
+  calibre \
+  keycastr \
+  wezterm \
+  kitty
