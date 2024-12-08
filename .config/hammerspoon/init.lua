352420b09d62257f96043b2b31b1e06f85e9ea35
jdhao <jdhao@hotmail.com>
2024-12-08 18:53:25 +0100

    Add Spoon: SpoonInstall and EmmyLua
    EmmyLua is used to add annotations for Hammerspoon and other Spoons, see
    also https://github.com/Hammerspoon/Spoons/pull/240

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -2,13 +2,23 @@
 
 -- require("./scripts/intput_source_switch")
 
-hs.loadSpoon("InputSourceSwitch")
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall:andUse("InputSourceSwitch")
 
 spoon.InputSourceSwitch:setApplications({
     ["WeChat"] = "Pinyin - Simplified",
+    ["Telegram"] = "Pinyin - Simplified",
     ["Raycast"] = "ABC",
     ["Google Chrome"] = "ABC",
     ["WezTerm"] = "ABC",
 })
 
 spoon.InputSourceSwitch:start()
+
+-- Show EmmyLua annotations for Hammerspoon itself and also for spoons
+spoon.SpoonInstall:andUse("EmmyLua")
+
+-- Note that we need to enabel notficaton for Hammerspoon in system "notification" setting,
+-- in oder to send notification below, otherwise, you see nothing.
+hs.notify.show("Hammerspoon started", "", "")
